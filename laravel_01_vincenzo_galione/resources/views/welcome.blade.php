a20e69e43ebb18bfc70f0367df888dbea7690456
VincenzoGalione <165274170+VincenzoGalione@users.noreply.github.com>
2024-06-11 13:40:38 +0200

    primo esercizo laravel

--- laravel_01_vincenzo_galione/resources/views/welcome.blade.php
+++ laravel_01_vincenzo_galione/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Selfwork</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   </head>
@@ -36,7 +36,7 @@
     <div class="container-fluid vh-100 bg-background">
         <div class="row h-75 justify-content-center align-items-center">
             <div class="col-12 py-5">
-                <h1 class="text-center diplay-4 title">
+                <h1 class="text-center display-1 text-color">
                     Benvenuti
                 </h1>
             </div>
